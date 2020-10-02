4a820298acb40d81e5fbec2ef0a259ac7b490ea6
rvk7895 <rkal2009@gmail.com>
2020-10-03 01:04:14 +0530

    added ClubStadiums.sql and Stadiums.sql

---
+++ SQL/Insert Scripts/ClubStadiums.sql
@@ -0,0 +1,7 @@
+INSERT INTO `CLUB-STADIUM` VALUES('Liverpool','Anfield');
+INSERT INTO `CLUB-STADIUM` VALUES('Manchester City','Etihad Stadium');
+INSERT INTO `CLUB-STADIUM` VALUES('Manchester United','Old Trafford');
+INSERT INTO `CLUB-STADIUM` VALUES('Newcastle',"St. James' Park");
+INSERT INTO `CLUB-STADIUM` VALUES('Southampton',"St. Mary's Stadium");
+INSERT INTO `CLUB-STADIUM` VALUES('Tottenham','Tottenham Hotspur Stadium');
+INSERT INTO `CLUB-STADIUM` VALUES('West Ham','London Stadium');
