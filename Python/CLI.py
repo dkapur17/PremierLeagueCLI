67595ebec2fdae63d23f5a3e0f7b68c015e4ffa1
Dhruv Kapur <dhruvkapur4@gmail.com>
2020-10-04 18:48:03 +0530

    Added Updation for all player stats

--- Python/CLI.py
+++ Python/CLI.py
@@ -258,8 +258,290 @@ def deleteManager():
 
 # Update Queries
 
+def updateGoalsScored(clubName, jerseyNum):
+    print("Update Goals Scored")
+    ip = input(
+        "Enter updated value for Goals Scored (enter negative value to cancel): ")
+    try:
+        if int(ip) < 0:
+            print("Task Cancelled.")
+            return
+    except Exception as e:
+        print("Invalid type for 'Goals Scored'. Task Cancelled.")
+        return
+    query = """UPDATE PLAYERS_OUTFIELD SET `Goals Scored` = %s WHERE `Club Name` = %s AND `Jersey Number` = %s"""
+    try:
+        cur.execute(query, (ip, clubName, jerseyNum))
+        connection.commit()
+    except Exception as e:
+        print("Unable to update entry.")
+        return
+    print("Entry Successfully Updated! The updated entry looks like: ")
+    query = """SELECT P.`First Name`, P.`Middle Name`, P.`Last Name`, PO.`Goals Scored`, PO.`Assists Provided`, PO.`Tackles Won`, P.`Market Value`, P.`Contract Duration` FROM PLAYERS AS P, PLAYERS_OUTFIELD AS PO WHERE P.`Club Name` = PO.`Club Name` AND P.`Jersey Number` = PO.`Jersey Number` AND P.`Club Name` = %s AND P.`Jersey Number` = %s;"""
+    cur.execute(query, (clubName, jerseyNum))
+    res = cur.fetchall()
+    print(tabulate(res, headers="keys", tablefmt='psql'))
+
+
+def updateAssistsProvided(clubName, jerseyNum):
+    print("Update Assists Provided")
+    ip = input(
+        "Enter updated value for Goals Scored (enter negative value to cancel): ")
+    try:
+        if int(ip) < 0:
+            print("Task Cancelled.")
+            return
+    except Exception as e:
+        print("Invalid type for 'Assists Provided'. Task Cancelled.")
+        return
+    query = """UPDATE PLAYERS_OUTFIELD SET `Assists Provided` = %s WHERE `Club Name` = %s AND `Jersey Number` = %s"""
+    try:
+        cur.execute(query, (ip, clubName, jerseyNum))
+        connection.commit()
+    except Exception as e:
+        print("Unable to update entry.")
+        return
+    print("Entry Successfully Updated! The updated entry looks like: ")
+    query = """SELECT P.`First Name`, P.`Middle Name`, P.`Last Name`, PO.`Goals Scored`, PO.`Assists Provided`, PO.`Tackles Won` , P.`Market Value`, P.`Contract Duration` FROM PLAYERS AS P, PLAYERS_OUTFIELD AS PO WHERE P.`Club Name` = PO.`Club Name` AND P.`Jersey Number` = PO.`Jersey Number` AND P.`Club Name` = %s AND P.`Jersey Number` = %s;"""
+    cur.execute(query, (clubName, jerseyNum))
+    res = cur.fetchall()
+    print(tabulate(res, headers="keys", tablefmt='psql'))
+
+
+def updateTacklesWon(clubName, jerseyNum):
+    print("Update Tackles Won")
+    ip = input(
+        "Enter updated value for Tackles Won (enter negative value to cancel): ")
+    try:
+        if int(ip) < 0:
+            print("Task Cancelled.")
+            return
+    except Exception as e:
+        print("Invalid type for 'Tackles Won'. Task Cancelled.")
+        return
+    query = """UPDATE PLAYERS_OUTFIELD SET `Tackles Won` = %s WHERE `Club Name` = %s AND `Jersey Number` = %s"""
+    try:
+        cur.execute(query, (ip, clubName, jerseyNum))
+        connection.commit()
+    except Exception as e:
+        print("Unable to update entry.")
+        return
+    print("Entry Successfully Updated! The updated entry looks like: ")
+    query = """SELECT P.`First Name`, P.`Middle Name`, P.`Last Name`, PO.`Goals Scored`, PO.`Assists Provided`, PO.`Tackles Won` , P.`Market Value`, P.`Contract Duration` FROM PLAYERS AS P, PLAYERS_OUTFIELD AS PO WHERE P.`Club Name` = PO.`Club Name` AND P.`Jersey Number` = PO.`Jersey Number` AND P.`Club Name` = %s AND P.`Jersey Number` = %s;"""
+    cur.execute(query, (clubName, jerseyNum))
+    res = cur.fetchall()
+    print(tabulate(res, headers="keys", tablefmt='psql'))
+
+
+def updateGoalsSaved(clubName, jerseyNum):
+    print("Update Goals Saved")
+    ip = input(
+        "Enter updated value for Saves (enter negative value to cancel): ")
+    try:
+        if int(ip) < 0:
+            print("Task Cancelled.")
+            return
+    except Exception as e:
+        print("Invalid type for 'Saves'. Task Cancelled.")
+        return
+    query = """UPDATE PLAYERS_GOALKEEPER SET `Saves` = %s WHERE `Club Name` = %s AND `Jersey Number` = %s"""
+    try:
+        cur.execute(query, (ip, clubName, jerseyNum))
+        connection.commit()
+    except Exception as e:
+        print("Unable to update entry.")
+        return
+    print("Entry Successfully Updated! The updated entry looks like: ")
+    query = """SELECT P.`First Name`, P.`Middle Name`, P.`Last Name`, PG.`Saves`, PG.`Clean Sheets`, P.`Market Value`, P.`Contract Duration` FROM PLAYERS AS P, PLAYERS_GOALKEEPER AS PG WHERE P.`Club Name` = PG.`Club Name` AND P.`Jersey Number` = PG.`Jersey Number` AND P.`Club Name` = %s AND P.`Jersey Number` = %s;"""
+    cur.execute(query, (clubName, jerseyNum))
+    res = cur.fetchall()
+    print(tabulate(res, headers="keys", tablefmt='psql'))
+
+
+def updateCleanSheets(clubName, jerseyNum):
+    print("Update Clean Sheets Kept")
+    ip = input(
+        "Enter updated value for Clean Sheets (enter negative value to cancel): ")
+    try:
+        if int(ip) < 0:
+            print("Task Cancelled.")
+            return
+    except Exception as e:
+        print("Invalid type for 'Clean Sheets'. Task Cancelled.")
+        return
+    query = """UPDATE PLAYERS_GOALKEEPER SET `Clean Sheets` = %s WHERE `Club Name` = %s AND `Jersey Number` = %s"""
+    try:
+        cur.execute(query, (ip, clubName, jerseyNum))
+        connection.commit()
+    except Exception as e:
+        print("Unable to update entry.")
+        return
+    print("Entry Successfully Updated! The updated entry looks like: ")
+    query = """SELECT P.`First Name`, P.`Middle Name`, P.`Last Name`, PG.`Saves`, PG.`Clean Sheets`, P.`Market Value`, P.`Contract Duration` FROM PLAYERS AS P, PLAYERS_GOALKEEPER AS PG WHERE P.`Club Name` = PG.`Club Name` AND P.`Jersey Number` = PG.`Jersey Number` AND P.`Club Name` = %s AND P.`Jersey Number` = %s;"""
+    cur.execute(query, (clubName, jerseyNum))
+    res = cur.fetchall()
+    print(tabulate(res, headers="keys", tablefmt='psql'))
+
+
+def updateMarketValue(clubName, jerseyNum, outfield):
+    print("Update Market Value")
+    ip = input(
+        "Enter updated Market Value (enter negative value to cancel): ")
+    try:
+        if int(ip) < 0:
+            print("Task Cancelled.")
+            return
+    except Exception as e:
+        print("Invalid type for 'Market Value'. Task Cancelled.")
+        return
+    query = """UPDATE PLAYERS SET `Market Value` = %s WHERE `Club Name` = %s AND `Jersey Number` = %s"""
+    try:
+        cur.execute(query, (ip, clubName, jerseyNum))
+        connection.commit()
+    except Exception as e:
+        print("Unable to update entry.")
+        return
+    print("Entry Successfully Updated! The updated entry looks like: ")
+    query = """SELECT P.`First Name`, P.`Middle Name`, P.`Last Name`, PO.`Goals Scored`, PO.`Assists Provided`, PO.`Tackles Won` , P.`Market Value`, P.`Contract Duration` FROM PLAYERS AS P, PLAYERS_OUTFIELD AS PO WHERE P.`Club Name` = PO.`Club Name` AND P.`Jersey Number` = PO.`Jersey Number` AND P.`Club Name` = %s AND P.`Jersey Number` = %s;"""
+    if not outfield:
+        query = """SELECT P.`First Name`, P.`Middle Name`, P.`Last Name`, PG.`Saves`, PG.`Clean Sheets`, P.`Market Value`, P.`Contract Duration` FROM PLAYERS AS P, PLAYERS_GOALKEEPER AS PG WHERE P.`Club Name` = PG.`Club Name` AND P.`Jersey Number` = PG.`Jersey Number` AND P.`Club Name` = %s AND P.`Jersey Number` = %s;"""
+    cur.execute(query, (clubName, jerseyNum))
+    res = cur.fetchall()
+    print(tabulate(res, headers="keys", tablefmt='psql'))
+
+
+def updateContractDuration(clubName, jerseyNum, outfield):
+    print("Update Contract Duration")
+    ip = input(
+        "Enter updated Contract Duration (enter negative value to cancel): ")
+    try:
+        if int(ip) < 0:
+            print("Task Cancelled.")
+            return
+    except Exception as e:
+        print("Invalid type for 'Contract Duration'. Task Cancelled.")
+        return
+    query = """UPDATE PLAYERS SET `Contract Duration` = %s WHERE `Club Name` = %s AND `Jersey Number` = %s"""
+    try:
+        cur.execute(query, (ip, clubName, jerseyNum))
+        connection.commit()
+    except Exception as e:
+        print("Unable to update entry.")
+        return
+    print("Entry Successfully Updated! The updated entry looks like: ")
+    query = """SELECT P.`First Name`, P.`Middle Name`, P.`Last Name`, PO.`Goals Scored`, PO.`Assists Provided`, PO.`Tackles Won` , P.`Market Value`, P.`Contract Duration` FROM PLAYERS AS P, PLAYERS_OUTFIELD AS PO WHERE P.`Club Name` = PO.`Club Name` AND P.`Jersey Number` = PO.`Jersey Number` AND P.`Club Name` = %s AND P.`Jersey Number` = %s;"""
+    if not outfield:
+        query = """SELECT P.`First Name`, P.`Middle Name`, P.`Last Name`, PG.`Saves`, PG.`Clean Sheets`, P.`Market Value`, P.`Contract Duration` FROM PLAYERS AS P, PLAYERS_GOALKEEPER AS PG WHERE P.`Club Name` = PG.`Club Name` AND P.`Jersey Number` = PG.`Jersey Number` AND P.`Club Name` = %s AND P.`Jersey Number` = %s;"""
+    cur.execute(query, (clubName, jerseyNum))
+    res = cur.fetchall()
+    print(tabulate(res, headers="keys", tablefmt='psql'))
+
+
+def dispatchOutfieldUpdate(ch, clubName, jerseyNum):
+    if ch == 1:
+        updateGoalsScored(clubName, jerseyNum)
+    elif ch == 2:
+        updateAssistsProvided(clubName, jerseyNum)
+    elif ch == 3:
+        updateTacklesWon(clubName, jerseyNum)
+    elif ch == 4:
+        updateMarketValue(clubName, jerseyNum, True)
+    elif ch == 5:
+        updateContractDuration(clubName, jerseyNum, True)
+
+
+def dispatchGoalkeeperUpdate(ch, clubName, jerseyNum):
+    if ch == 1:
+        updateGoalsSaved(clubName, jerseyNum)
+    elif ch == 2:
+        updateCleanSheets(clubName, jerseyNum)
+    elif ch == 3:
+        updateMarketValue(clubName, jerseyNum, False)
+    elif ch == 4:
+        updateContractDuration(clubName, jerseyNum, False)
+
+
 def updatePlayerStats():
-    return
+    print("Update Player Stats: ")
+    print()
+    clubName = input("Enter Club Name: ")
+    jerseyNum = input("Enter Jersey Number: ")
+    query = """SELECT P.`First Name`, P.`Middle Name`, P.`Last Name`, PO.`Goals Scored`, PO.`Assists Provided`, PO.`Tackles Won`,  P.`Market Value`, P.`Contract Duration` FROM PLAYERS AS P, PLAYERS_OUTFIELD AS PO WHERE P.`Club Name` = PO.`Club Name` AND P.`Jersey Number` = PO.`Jersey Number` AND P.`Club Name` = %s AND P.`Jersey Number` = %s;"""
+    cur.execute(query, (clubName, jerseyNum))
+    res = cur.fetchall()
+    if len(res):
+        ch = 1e9
+        while ch != 6:
+            sp.call('clear', shell=True)
+            print("Selected Player: ")
+            cur.execute(query, (clubName, jerseyNum))
+            res = cur.fetchall()
+            print(tabulate(res, headers="keys", tablefmt='psql'))
+            print()
+            print("Choose one of the options: ")
+            print("1. Goals Scored")
+            print("2. Assists Provided")
+            print("3. Tackles Won")
+            print("4. Market Value")
+            print("5. Contract Duration")
+            print("6. Back")
+            while ch > 6 or ch < 1:
+                try:
+                    ch = int(input("Enter a choice: "))
+                except ValueError:
+                    continue
+                if ch == 6:
+                    return True
+                elif ch > 6 or ch < 1:
+                    continue
+                else:
+                    sp.call('clear', shell=True)
+                    back = False
+                    back = dispatchOutfieldUpdate(ch, clubName, jerseyNum)
+                    if not back:
+                        input("Enter any key to continue")
+                    ch = 1e9
+                    break
+
+    else:
+        query = """SELECT P.`First Name`, P.`Middle Name`, P.`Last Name`, PG.`Saves`, PG.`Clean Sheets`, P.`Market Value`, P.`Contract Duration` FROM PLAYERS AS P, PLAYERS_GOALKEEPER AS PG WHERE P.`Club Name` = PG.`Club Name` AND P.`Jersey Number` = PG.`Jersey Number` AND P.`Club Name` = %s AND P.`Jersey Number` = %s;"""
+        cur.execute(query, (clubName, jerseyNum))
+        res = cur.fetchall()
+        if len(res):
+            ch = 1e9
+            while ch != 5:
+                sp.call('clear', shell=True)
+                print("Selected Player: ")
+                cur.execute(query, (clubName, jerseyNum))
+                res = cur.fetchall()
+                print(tabulate(res, headers="keys", tablefmt='psql'))
+                print()
+                print("Choose one of the options: ")
+                print("1. Saves Made")
+                print("2. Clean Sheets")
+                print("3. Market Value")
+                print("4. Contract Duration")
+                print("5. Back")
+                while ch > 5 or ch < 1:
+                    try:
+                        ch = int(input("Enter a choice: "))
+                    except ValueError:
+                        continue
+                    if ch == 5:
+                        return True
+                    elif ch > 5 or ch < 1:
+                        continue
+                    else:
+                        sp.call('clear', shell=True)
+                        back = False
+                        back = dispatchGoalkeeperUpdate(
+                            ch, clubName, jerseyNum)
+                        if not back:
+                            input("Enter any key to continue")
+                        ch = 1e9
+                        break
+        else:
+            print("No such player")
 
 
 def updateStadiumCapacity():
@@ -297,6 +579,40 @@ def updateStadiumCapacity():
 
 
 def updateFixtureKits():
+    print("Update Fixture Kits: ")
+    print()
+    matchId = input("Match ID: ")
+
+    query = """SELECT * FROM FIXTURES WHERE `Match ID` = %s;"""
+    cur.execute(query, (matchId))
+    table = cur.fetchall()
+    if len(table):
+        print(tabulate(table, headers="keys", tablefmt='psql'))
+    else:
+        print("No such fixture")
+        return
+
+    res = input("Are you sure you want to update the above entry? [y/n] ")
+    if res == "y" or res == "Y":
+        newHomeKit = input(
+            "Enter Home Team Kit for this Fixture (Home, Away or Alternate): ")
+        newAwayKit = input(
+            "Enter Away Team Kit for this Fixture (Home, Away or Alternate): ")
+        try:
+            query = """UPDATE FIXTURES SET `Home Club Kit` = %s, `Away Club Kit` = %s WHERE `Match ID`= %s;"""
+            cur.execute(query, (newHomeKit, newAwayKit, matchId))
+            connection.commit()
+        except Exception as e:
+            print(
+                "Unable to update. Potentially invalid type for 'Home Team Kit' or 'Away Team Kit'")
+            return
+        print("Updated the requested entry. Updated entry looks like: ")
+        query = """SELECT * FROM FIXTURES WHERE `Match ID` = %s;"""
+        cur.execute(query, (matchId))
+        table = cur.fetchall()
+        print(tabulate(table, headers="keys", tablefmt='psql'))
+    else:
+        print("Aborted Update.")
     return
 
 
@@ -324,7 +640,7 @@ def dispatchDelete(ch):
 
 def dispatchUpdate(ch):
     if ch == 1:
-        updatePlayerStats()
+        return updatePlayerStats()
     elif ch == 2:
         updateStadiumCapacity()
     elif ch == 3:
@@ -404,7 +720,7 @@ def update():
         print("Delete Entry from the Database")
         print()
         print("Choose one of the options: ")
-        print("1. Update an Existing Entry:")
+        print("1. Update a Player's Stats")
         print("2. Update Stadium Capacity")
         print("3. Update Fixture Kits")
         print("4. Back")
